8f4de1bb9fa8f3fba0c1763633fbe6e1f3daf61f
Alex <alexey94709470@gmail.com>
2014-12-18 13:33:00 +0400

    Created project

---
+++ MyBlog/Areas/Founder/FounderAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MyBlog.Areas.Founder
+{
+    public class FounderAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Founder";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Founder_default",
+                "Founder/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
